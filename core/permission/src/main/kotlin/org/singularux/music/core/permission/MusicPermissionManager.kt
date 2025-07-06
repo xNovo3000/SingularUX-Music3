fe05ac46eabdf21213236e486686d01a941beeb9
xNovo3000 <fontana.ciro.2001@gmail.com>
2025-07-06 12:22:09 +0200

    Finished permission module

---
+++ core/permission/src/main/kotlin/org/singularux/music/core/permission/MusicPermissionManager.kt
@@ -0,0 +1,17 @@
+package org.singularux.music.core.permission
+
+interface MusicPermissionManager {
+
+    fun hasPermission(permission: MusicPermission): Boolean
+
+    fun getPermissionString(permission: MusicPermission): String
+
+    fun hasPermissions(permissions: List<MusicPermission>): List<Boolean> {
+        return permissions.map { hasPermission(it) }
+    }
+
+    fun getPermissionStrings(permissions: List<MusicPermission>): List<String> {
+        return permissions.map { getPermissionString(it) }
+    }
+
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -22,3 +22,4 @@ dependencyResolutionManagement {
 rootProject.name = "Music"
 include(":app")
 include(":core:ui")
+include(":core:permission")
