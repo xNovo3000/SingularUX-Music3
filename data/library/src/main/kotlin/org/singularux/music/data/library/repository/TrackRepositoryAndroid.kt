a636b4a519275c849ee923fc0ce433e2b69a8465
xNovo3000 <fontana.ciro.2001@gmail.com>
2025-07-06 14:19:41 +0200

    Finished track repository, starting first UI

--- data/library/src/main/kotlin/org/singularux/music/data/library/repository/TrackRepositoryAndroid.kt
+++ data/library/src/main/kotlin/org/singularux/music/data/library/repository/TrackRepositoryAndroid.kt
@@ -3,12 +3,16 @@ package org.singularux.music.data.library.repository
 import android.content.Context
 import android.provider.MediaStore
 import android.util.Log
+import androidx.core.database.getLongOrNull
+import androidx.core.database.getStringOrNull
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.android.scopes.ActivityRetainedScoped
 import org.singularux.music.core.permission.MusicPermission
 import org.singularux.music.core.permission.MusicPermissionManager
 import org.singularux.music.data.library.entity.TrackEntity
 import javax.inject.Inject
+import kotlin.time.Duration.Companion.milliseconds
+import androidx.core.net.toUri
 
 @ActivityRetainedScoped
 class TrackRepositoryAndroid @Inject constructor(
@@ -17,7 +21,9 @@ class TrackRepositoryAndroid @Inject constructor(
 ) : TrackRepository {
 
     companion object {
+
         private const val TAG = "TrackRepositoryAndroid"
+
         private val GET_ALL_URI = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
         private val GET_ALL_PROJECTION = arrayOf(
             MediaStore.Audio.Media._ID,
@@ -26,6 +32,13 @@ class TrackRepositoryAndroid @Inject constructor(
             MediaStore.Audio.Media.ALBUM_ID,
             MediaStore.Audio.Media.DURATION
         )
+        private const val GET_ALL_SELECTION = MediaStore.Audio.Media.IS_TRASHED + " = ?" +
+                MediaStore.Audio.Media.IS_MUSIC + " = ?"
+        private val GET_ALL_SELECTION_ARGS = arrayOf("0", "1")
+        private const val GET_ALL_SORT_ORDER = MediaStore.Audio.Media.DEFAULT_SORT_ORDER
+
+        private const val ARTWORK_URI_STRING = "content://media/external/audio/albumart"
+
     }
 
     override suspend fun getAll(): List<TrackEntity> {
@@ -33,10 +46,31 @@ class TrackRepositoryAndroid @Inject constructor(
         // If not present, return empty list
         // If present, query MediaStore
         if (!musicPermissionManager.hasPermission(MusicPermission.READ_MUSIC)) {
-            Log.i(TAG, "getAll(): missing permission READ_MUSIC")
+            Log.d(TAG, "getAll(): missing permission READ_MUSIC")
             return emptyList()
         }
-        // context.contentResolver.query()
+        return context.contentResolver.query(
+            GET_ALL_URI, GET_ALL_PROJECTION,
+            GET_ALL_SELECTION, GET_ALL_SELECTION_ARGS,
+            GET_ALL_SORT_ORDER
+        ).use { cursor ->
+            val result = mutableListOf<TrackEntity>()
+            while (cursor?.moveToNext() == true) {
+                val albumId = cursor.getLongOrNull(5)
+                result.add(
+                    element = TrackEntity(
+                        id = cursor.getLong(0),
+                        title = cursor.getStringOrNull(1) ?: cursor.getString(2),
+                        artistId = cursor.getLongOrNull(3),
+                        artistName = cursor.getStringOrNull(4),
+                        albumId = albumId,
+                        artwork = if (albumId != null) "${ARTWORK_URI_STRING}/${albumId}".toUri() else null,
+                        duration = cursor.getLong(6).milliseconds
+                    )
+                )
+            }
+            result
+        }
     }
 
 }
