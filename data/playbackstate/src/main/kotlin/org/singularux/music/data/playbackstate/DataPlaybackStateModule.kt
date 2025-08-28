1c02370a7583123c2a93d48f786e837a370ebd06
xNovo3000 <fontana.ciro.2001@gmail.com>
2025-08-28 13:02:27 +0200

    Database must be a singleton because workers need them

--- data/playbackstate/src/main/kotlin/org/singularux/music/data/playbackstate/DataPlaybackStateModule.kt
+++ data/playbackstate/src/main/kotlin/org/singularux/music/data/playbackstate/DataPlaybackStateModule.kt
@@ -1,10 +1,27 @@
 package org.singularux.music.data.playbackstate
 
+import android.content.Context
+import androidx.room.Room
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import org.singularux.music.data.playbackstate.timeline.TimelineDatabase
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 class DataPlaybackStateModule {
+
+    @Provides
+    @Singleton
+    fun providesTimelineDatabase(@ApplicationContext context: Context): TimelineDatabase {
+        return Room.databaseBuilder(
+            context = context,
+            klass = TimelineDatabase::class.java,
+            name = "timeline.sql"
+        ).build()
+    }
+
 }
